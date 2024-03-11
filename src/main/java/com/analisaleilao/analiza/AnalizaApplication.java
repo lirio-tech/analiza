5f1c4b1710893ff2eb54952bee40ec260dff6861
Diego Lírio <diegolirio.dl@gmail.com>
2024-03-10 18:58:44 -0700

    First_Commmit

---
+++ src/main/java/com/analisaleilao/analiza/AnalizaApplication.java
@@ -0,0 +1,13 @@
+package com.analisaleilao.analiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalizaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analisaleilao/analiza/AnalizaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analisaleilao.analiza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
